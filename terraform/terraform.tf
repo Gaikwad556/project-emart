90040e10038e5c886e052dae7e80c46b69969d56
Gaikwad556 <gamingonearth556@gmail.com>
2024-07-31 11:13:18 +0530

    update

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -24,7 +24,7 @@ data "aws_secretsmanager_secret_version" "my_private_key_sonarqube" {
 
 terraform {
   backend "s3" {
-    bucket = "project-vprofile-556"
+    bucket = "project-emart"
     key    = "file/terraform.tfstate"
     region = "us-west-1"
   }
